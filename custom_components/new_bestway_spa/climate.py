3a9a2d462b114a6a22c020cb7eb8ba43b417567c
David Johnson <philotic_node@protonmail.com>
2025-10-02 00:31:36 -0400

    Fix translations

--- custom_components/new_bestway_spa/climate.py
+++ custom_components/new_bestway_spa/climate.py
@@ -23,6 +23,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
 
 
 class BestwaySpaThermostat(CoordinatorEntity, ClimateEntity):
+    has_entity_name = True
     _attr_hvac_modes = [HVACMode.OFF, HVACMode.HEAT]
     _attr_supported_features = ClimateEntityFeature.TARGET_TEMPERATURE
     _attr_temperature_unit = "°C"
@@ -32,7 +33,8 @@ class BestwaySpaThermostat(CoordinatorEntity, ClimateEntity):
     def __init__(self, coordinator, api, title, device_id, hass):
         super().__init__(coordinator)
         self._api = api
-        self._attr_name = f"{title} Thermostat"
+        self._attr_translation_key = "thermostat"
+        self._attr_translation_placeholders = {"name": f"{title} Thermostat"}
         self._attr_unique_id = f"{device_id}_thermostat"
         self._device_id = device_id
         self.hass = hass
@@ -41,7 +43,8 @@ class BestwaySpaThermostat(CoordinatorEntity, ClimateEntity):
     def device_info(self):
         return {
             "identifiers": {(DOMAIN, self._device_id)},
-            "name": self._attr_name.split(" ")[0],
+            "translation_key": self._attr_translation_key,
+            "translation_placeholders": self._attr_translation_placeholders,
             "manufacturer": "Bestway",
             "model": "Spa",
             "sw_version": self.hass.data[DOMAIN].get("manifest_version", "unknown")
